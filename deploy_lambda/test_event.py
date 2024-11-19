f4bd082aeab2de6f9c00f7e5efbd670fad23bbc0
Reza Sadoughian <sezar543@yahoo.com>
2024-11-18 19:53:27 -0800

    revised build_deploy_mlflow.yml

---
+++ deploy_lambda/test_event.py
@@ -0,0 +1,4 @@
+import json
+with open('event.json') as f:
+    data = json.load(f)
+print(data)
---
+++ deploy_mlflow/test_mlflow_path.py
@@ -0,0 +1,4 @@
+from utils import get_mlflow_db_path  # Adjust import path if utils is in a subfolder
+
+# Print the MLflow DB path
+print("MLflow DB path:", get_mlflow_db_path())
